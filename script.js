d872411fcae1eb14c9a9a47b903a673168224f6e
vibhatsu08 <mistryvedant17@gmail.com>
2022-09-14 06:38:11 +0530

    Implemented form validation for the project, so if the user input has only whitespaces, the trim() function will remove those whitespaces and will check for actual content, and if the input has no non space characters, then the screen gives an alert/prompt to add a valid input.

--- script.js
+++ script.js
@@ -12,64 +12,69 @@ window.addEventListener (
                 // variable to store the task input
                 const taskInput = addTask.value;
 
-                // for task div.
-                const task = document.createElement('div');
-                task.classList.add('task');
+                if (taskInput.trim() == "") {
+                    alert("enter valid input!");
+                }
+                else {
+                    // for task div.
+                    const task = document.createElement('div');
+                    task.classList.add('task');
 
-                const taskText = document.createElement('div');
-                taskText.classList.add('taskText');
-                taskText.textContent = taskInput;
-                task.appendChild(taskText);
+                    const taskText = document.createElement('div');
+                    taskText.classList.add('taskText');
+                    taskText.textContent = taskInput;
+                    task.appendChild(taskText);
 
-                // for task buttons div, appending all the buttons to the parent buttons div
-                const taskButtons = document.createElement('div');
-                taskButtons.classList.add('taskButtons');
-                const taskEditButton = document.createElement('button'); 
-                taskEditButton.classList.add('taskEditButton');
-                taskEditButton.textContent = "edit";
-                const taskDoneButton = document.createElement('button');
-                taskDoneButton.classList.add('taskDoneButton');
-                taskDoneButton.textContent = "done";
-                const taskDeleteButton = document.createElement('button');
-                taskDeleteButton.classList.add('taskDeleteButton');
-                taskDeleteButton.textContent = "delete";
+                    // for task buttons div, appending all the buttons to the parent buttons div
+                    const taskButtons = document.createElement('div');
+                    taskButtons.classList.add('taskButtons');
+                    const taskEditButton = document.createElement('button'); 
+                    taskEditButton.classList.add('taskEditButton');
+                    taskEditButton.textContent = "edit";
+                    const taskDoneButton = document.createElement('button');
+                    taskDoneButton.classList.add('taskDoneButton');
+                    taskDoneButton.textContent = "done";
+                    const taskDeleteButton = document.createElement('button');
+                    taskDeleteButton.classList.add('taskDeleteButton');
+                    taskDeleteButton.textContent = "delete";
 
-                taskButtons.appendChild(taskEditButton);
-                taskButtons.appendChild(taskDoneButton);
-                taskButtons.appendChild(taskDeleteButton);
-                task.appendChild(taskButtons);
-                tasksParentDiv.appendChild(task);
-                form.reset();
+                    taskButtons.appendChild(taskEditButton);
+                    taskButtons.appendChild(taskDoneButton);
+                    taskButtons.appendChild(taskDeleteButton);
+                    task.appendChild(taskButtons);
+                    tasksParentDiv.appendChild(task);
+                    form.reset();
 
-                taskEditButton.addEventListener (
-                    'click', function () {
-                        taskText.contentEditable = true;
-                        taskText.style.color = '#FC5130';
-                        taskEditButton.textContent = 'save';
-                    }
-                );
-                taskEditButton.addEventListener (
-                    'dblclick', function () {
-                        taskText.contentEditable = false;
-                        taskText.style.color = '#FAF0CA';
-                        taskEditButton.textContent = 'edit';
-                    }
-                );
-                taskDoneButton.addEventListener (
-                    'click', function () {
-                        task.style.backgroundColor = '#00A676';
-                    }
-                );
-                taskDoneButton.addEventListener (
-                    'dblclick', function () {
-                        task.style.backgroundColor = '#FFC857';
-                    }
-                );
-                taskDeleteButton.addEventListener (
-                    'click', function () {
-                        tasksParentDiv.removeChild(task);
-                    }
-                );
+                    taskEditButton.addEventListener (
+                        'click', function () {
+                            taskText.contentEditable = true;
+                            taskText.style.color = '#FC5130';
+                            taskEditButton.textContent = 'save';
+                        }
+                    );
+                    taskEditButton.addEventListener (
+                        'dblclick', function () {
+                            taskText.contentEditable = false;
+                            taskText.style.color = '#FAF0CA';
+                            taskEditButton.textContent = 'edit';
+                        }
+                    );
+                    taskDoneButton.addEventListener (
+                        'click', function () {
+                            task.style.backgroundColor = '#00A676';
+                        }
+                    );
+                    taskDoneButton.addEventListener (
+                        'dblclick', function () {
+                            task.style.backgroundColor = '#FFC857';
+                        }
+                    );
+                    taskDeleteButton.addEventListener (
+                        'click', function () {
+                            tasksParentDiv.removeChild(task);
+                        }
+                    );
+                }
             }
         )
     }
